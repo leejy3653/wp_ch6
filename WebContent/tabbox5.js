7ec6e5589a8fcb02ee4ccc45a83e4a8c1dd5e695
ljy <leejy3653@naver.com>
2019-10-30 19:35:31 +0900

    init commint

---
+++ WebContent/tabbox5.js
@@ -0,0 +1,15 @@
+var tabBox = {
+	onTabClicked : function(event) {
+		var lisSelected = document.getElementsByClassName("selected");
+		(lisSelected.length == 1) && (lisSelected[0].className = "");
+		this.className = "selected";
+	},
+	init : function() {
+		var div = document.getElementsByClassName("tab-box")[0];
+		var ul = div.childNodes[1];
+		var lis = ul.getElementsByTagName("li");
+		for (var i = 0; i < lis.length; i++) {
+			lis[i].addEventListener("click", this.onTabClicked);
+		}
+	}
+}
